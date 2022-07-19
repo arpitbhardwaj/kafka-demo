6173d01e02852daf6d9e556cb21caa6db02b4b37
arbhard2 <arbhard2@cisco.com>
2022-07-19 09:27:31 +0530

    Kafka Producer Demo

---
+++ kafka-basics/src/main/java/com/ab/kafkabasics/producer/ProducerDemo.java
@@ -0,0 +1,40 @@
+package com.ab.kafkabasics.producer;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+    public static void main(String[] args) {
+        log.info("I am a kafka producer");
+
+        //create producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG , "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG , StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG , StringSerializer.class.getName());
+
+        //create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        //create a producer record
+        ProducerRecord<String,String> producerRecord =
+                new ProducerRecord<>("demo_java", "hello ProducerDemo");
+
+        //send data - async operation
+        producer.send(producerRecord);
+
+        //flush data - synchronous opeartion
+        producer.flush();
+
+        //close the producer
+        producer.close();
+    }
+}
---
+++ kafka-basics/src/main/java/com/ab/kafkabasics/producer/ProducerDemoWithCallback.java
@@ -0,0 +1,60 @@
+package com.ab.kafkabasics.producer;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemoWithCallback {
+
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemoWithCallback.class.getSimpleName());
+    public static void main(String[] args) {
+        log.info("I am a kafka producer");
+
+        //create producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG , "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG , StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG , StringSerializer.class.getName());
+
+        //create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        for (int i = 0; i < 10; i++) {
+            //create a producer record
+            ProducerRecord<String,String> producerRecord =
+                    new ProducerRecord<>("demo_java", "hello ProducerDemoWithCallback " + i);
+
+            //send data - async operation
+            producer.send(producerRecord, new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata metadata, Exception exception) {
+                    //executes every time a record is successfully sent or an exception thrown
+                    if (exception == null){
+                        log.info("Received metadata/ \n" +
+                                "Topic: " + metadata.topic() + "\n" +
+                                "Partition: " + metadata.partition() + "\n" +
+                                "Offset: " + metadata.offset() + "\n" +
+                                "Timestamp: " + metadata.timestamp());
+                    }else {
+                        log.error("Error while producing: " + exception.getMessage(), exception);
+                    }
+                }
+            });
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        //flush data - synchronous operation
+        producer.flush();
+
+        //close the producer
+        producer.close();
+    }
+}
